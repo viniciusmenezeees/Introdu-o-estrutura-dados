0137aecb2eb8671b7c44bd9be49262a01f70bc94
viniciusmenezeees <vinicius.m.oliveira@uni9.edu.br>
2025-04-10 00:29:25 +0000

    exemplo array

---
+++ main.js
@@ -0,0 +1,14 @@
+let aluno_nota_av1 = 8;
+let aluno_nota_av2 = 9.6;
+let aluno_media = (aluno_nota_av1 + aluno_nota_av2) / 2;
+
+console.log("Exemplo com arrays");
+let notas_aluno = [8, 9.6];
+console.log(notas_aluno);
+
+console.log("AV1: " + notas_aluno[0]);
+console.log("AV2: " + notas_aluno[1]);
+notas_aluno[2]= (notas_aluno[0]  + notas_aluno[0] + notas_aluno[1]) / 2;
+console.log("Média: " + notas_aluno[2]);
+console.log("Todas: " + notas_aluno);
+
